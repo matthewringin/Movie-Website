0315ddfee491e5dd7084f3f67b6804640944ed87
matthewringin <42225898+matthewringin@users.noreply.github.com>
2018-08-15 15:53:57 +1000

    Initial commit
    Copied files over from different location.
    Initial commit.

---
+++ WebApplication/Controllers/MoviesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+using WebApplication.ViewModels;
+
+namespace WebApplication.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "John Smith", Id = 1},
+                new Customer { Name = "Mary Williams", Id = 2}
+            };
+
+            var viewModel = new RandomMovieViewModel 
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+    }
+
+}
